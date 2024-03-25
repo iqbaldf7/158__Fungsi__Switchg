cf9e885dd484240a8a2112bd36479063d12bf0e1
iqbaldf7 <Iqbaldathonah10@gmail.com>
2024-03-25 15:21:48 +0700

    Add project files.

---
+++ 158__Fungsi__Switchg/158__Fungsi__Switchg.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int bilSatu, bilDua;
+
+void input()
+{
+    cout << "Masukan Bilangan pertama = ";
+    cin >> bilSatu;
+    cout << "Masukan Bilangan Kedua = ";
+    cin >> bilDua;
+}
